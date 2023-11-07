23e95049fde651caf42fda913a36187bc96930b8
Muzza1103 <145678126+Muzza1103@users.noreply.github.com>
2023-11-07 17:28:33 +0100

    Add files via upload

---
+++ Projet_PAA/Projet_PAA/src/up/mi/Ca.java
@@ -0,0 +1,13 @@
+package up.mi;
+
+public class Ca {
+	int nbrVille;
+	boolean [][] matriceAdj;
+	
+	public Ca(int nbrVille) {
+		this.nbrVille = nbrVille;
+		matriceAdj = new boolean[nbrVille][nbrVille];
+	}
+	
+	
+}
