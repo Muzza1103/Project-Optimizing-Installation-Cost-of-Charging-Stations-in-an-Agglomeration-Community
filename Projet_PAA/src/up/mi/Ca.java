d071d827b0761b54db986392e363089e1a9736f5
rayan <rayan@192.168.0.29>
2023-11-09 23:02:45 +0100

    Correction erreur dans la methode ajouterZoneDeRecharge changement du if ==1 a ==-1 et code de la methode retirerZoneDeRecharge

--- Projet_PAA/src/up/mi/Ca.java
+++ Projet_PAA/src/up/mi/Ca.java
@@ -4,16 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Ca {
+	
+    // Attributs de la classe
 	int nbrVille;
 	boolean [][] matriceAdj;
 	private List<Ville> villes;
 	
+    // Constructeur
 	public Ca(int nbrVille) {
+		//nombre de ville
 		this.nbrVille = nbrVille;
 		villes = new ArrayList<>();
 		matriceAdj = new boolean[nbrVille][nbrVille];
 	}
-	
+	//Ajout d'une ville
 	public void ajouterVille(String nom){
 		if (villes.size() < nbrVille) {
             villes.add(new Ville(nom));
@@ -22,7 +26,7 @@ public class Ca {
         System.out.println("Le nombre maximum de villes est atteint (" + nbrVille + "). Impossible d'ajouter : " + nom);
     	}
 	}
-	
+	//ajout d'une route
 	public void ajouterRoute(String A, String B) {
 		int i=-1;
 		int j=-1;
@@ -46,7 +50,7 @@ public class Ca {
 			System.out.println("Erreur : "+e.getMessage());
 		}
 	}
-	
+	// Ajout d'un zone de recharge 
 	public void ajouterZoneDeRecharge(String A) {
 		try {
 			int i=-1;
@@ -56,7 +60,7 @@ public class Ca {
 					villes.get(k).ajouterZone();
 				}
 			}
-			if (i==1) {
+			if (i==-1) {
 				throw new IllegalArgumentException("La ville "+A+" n'existe pas !");
 			}
 		}catch(IllegalArgumentException e ) {
@@ -64,8 +68,23 @@ public class Ca {
 		}
 	}
 	
-	
+	// Suppression d'une zone de recharge
 	public void retirerZoneDeRecharge(String A) {
-		
+		// Tente de retirer une zone de recharge de la ville A
+		try {
+			int i=-1;
+			for (int k = 0; k<villes.size();k++) {
+				if (villes.get(k).getNom().equals(A)){
+					i = k;
+					villes.get(k).retirerZone();
+				}
+			}
+			//affiche un message d'erreur si la ville n'exsite pas 
+			if (i==-1) {
+				throw new IllegalArgumentException("La ville "+A+" n'existe pas !");
+			}
+		}catch(IllegalArgumentException e ) {
+			System.out.println("Erreur : "+e.getMessage());
+		}
 	}
 }
