b21de426f27ec727e6a235b3b3bcbf65e385dce2
Muzza1103 <145678126+Muzza1103@users.noreply.github.com>
2023-11-11 23:20:57 +0100

    Update Menu.java

--- Projet_PAA/src/up/mi/Menu.java
+++ Projet_PAA/src/up/mi/Menu.java
@@ -6,9 +6,10 @@ public class Menu {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("Entrer le nombre de villes que vous souhaitez ajouté ( doit être inférieur à 26 ) : ");
+        int nbrVille = sc.nextInt();
         // Initialisation de l'objet 'Ca' pour gérer les données des villes et des routes
-        Ca ca = new Ca();
+        Ca ca = new Ca(nbrVille);
      // Initialisation du choix
         int choix = 0;
 
@@ -22,42 +23,55 @@ public class Menu {
 
             switch (choix) {
                 case 1:
-                    System.out.println("Entrez le nom de la ville :\n");
-                    String nomVille = sc.nextLine().toUpperCase();//Affin d'eviter les pb d'oublie de maj ect...
-                    
-                    // Appel de la méthode pour ajouter une ville
-                    ca.ajouterVille(nomVille);
-                    break;
-                case 2:
-                    System.out.println("Entrez le nom de la ville A qui va vers la ville B.\n");
+                    System.out.println("\nEntrez le nom de la ville A qui relie la ville B.");
                     String a = sc.nextLine().toUpperCase();//Affin d'eviter les pb d'oublie de maj ect...
+                    System.out.println("Entrer le nom de la ville B");
                     String b = sc.nextLine().toUpperCase();
   
                     // Appel de la méthode pour ajouter une route entre deux villes
                     ca.ajouterRoute(a, b);
                     break;
-                case 3:
-                    System.out.println("Entrez le nom de la ville où vous souhaitez ajouter une borne de recharge.\n");
-                    String ajout = sc.nextLine().toUpperCase(); //Affin d'eviter les pb d'oublie de maj ect...
-                    
-                    // Appel de la méthode pour ajouter une zone de recharge dans une ville
-                    ca.ajouterZoneDeRecharge(ajout);
-                    break;
-                case 4:
-                    System.out.println("Entrez le nom de la ville où vous souhaitez retirer une borne de recharge.\n");
-                    String retirer = sc.nextLine().toUpperCase();//Affin d'eviter les pb d'oublie de maj ect...
-
-                    
-                    // Appel de la méthode pour retirer une zone de recharge d'une ville
-                    ca.retirerZoneDeRecharge(retirer);
-                    break;
-                case 0:
+                case 2:
                     // Sortir de la boucle et terminer le programme
                     break;
                 default:
                     System.out.println("Erreur : l'option " + choix + " n'est pas valable.");
             }
-        } while (choix != 4);
+        } while (choix != 2);
+        	
+        do {
+        	// Afficher le menu des options disponibles
+            menu2();
+            System.out.println("\nListe des villes possédant des zones de recharge :");
+            ca.afficheVillesAvecZone();
+            
+            // Lire le choix de l'utilisateur
+            choix = sc.nextInt();
+            sc.nextLine(); // Consommer la nouvelle ligne après avoir lu un entier
+
+            switch (choix) {
+            	case 1:
+            		System.out.println("\nEntrez le nom de la ville où vous souhaitez ajouter une borne de recharge.");
+            		String ajout = sc.nextLine().toUpperCase(); //Affin d'eviter les pb d'oublie de maj ect...
+                
+            		// Appel de la méthode pour ajouter une zone de recharge dans une ville
+            		ca.ajouterZoneDeRecharge(ajout);
+            		break;
+            	case 2:
+            		System.out.println("Entrez le nom de la ville où vous souhaitez retirer une borne de recharge.");
+            		String retirer = sc.nextLine().toUpperCase();//Affin d'eviter les pb d'oublie de maj ect...
+
+                
+            		// Appel de la méthode pour retirer une zone de recharge d'une ville
+            		ca.retirerZoneDeRecharge(retirer);
+            		break;
+            	case 3:
+            		// Sortir de la boucle et terminer le programme
+            		break;
+            	default:
+            		System.out.println("Erreur : l'option " + choix + " n'est pas valable.");
+            }
+        } while (choix != 3);
 
         // Ferme le scanner
         sc.close();
@@ -65,11 +79,14 @@ public class Menu {
 
     // Méthode pour afficher le menu des options
     private static void menu1() {
-        System.out.println("Que souhaitez vous faire ?");
-        System.out.println("1 - Ajouter une ville");
-        System.out.println("2 - Ajouter une route");
-        System.out.println("3 - Ajouter une zone de recharge");
-        System.out.println("4 - Retirer une zone de recharge");
-        System.out.println("0 - Fin");
+        System.out.println("\n1 - Ajouter une route;");
+        System.out.println("2 - Fin.");
     }
+    
+    private static void menu2() {
+    	System.out.println("\n1 - Ajouter une zone de recharge;");
+        System.out.println("2 - Retirer une zone de recharge;");
+        System.out.println("3 - Fin.");
+    }
+    
 }
