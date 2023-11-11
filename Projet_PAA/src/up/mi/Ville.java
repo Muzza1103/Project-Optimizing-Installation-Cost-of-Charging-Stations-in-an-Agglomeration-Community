3cd7ab440b4ff12cf3a2b3ca37a384d0b949225b
rayan <rayan@localhost>
2023-11-12 00:37:56 +0100

    commenter les methode

--- Projet_PAA/src/up/mi/Ville.java
+++ Projet_PAA/src/up/mi/Ville.java
@@ -1,40 +1,39 @@
 package up.mi;
 
 public class Ville {
-	private String nom;
-	private boolean zoneDeRecharge;
-	
-	public Ville(String nom) {
-		this.nom = nom;
-		zoneDeRecharge = true;
-	}
-	
-	public String getNom() {
-		return nom;
-	}
-	
-	public boolean getZone() {
-		return zoneDeRecharge;
-	}
-	public boolean getZoneSans() {
-		return !zoneDeRecharge;
-	}
-	
-	
-	public void ajouterZone() {
-		if(zoneDeRecharge) {
-			System.out.println("Il existe déjà une zone de recharge dans la ville "+nom);
-		}else {
-			zoneDeRecharge = true;
-		}
-	}
-	
-	public void retirerZone() {
-		if(!zoneDeRecharge) {
-			System.out.println("Il n'existe pas de zone de recharge dans la ville : "+nom);
-		}else {
-			zoneDeRecharge = false;
-		}
-	}
-}
+    private String nom;
+    private boolean zoneDeRecharge;
 
+    public Ville(String nom) {
+        this.nom = nom;
+        zoneDeRecharge = true; // Par défaut, une nouvelle ville a une zone de recharge.
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public boolean getZone() {
+        return zoneDeRecharge;
+    }
+    
+    public boolean getZoneSans() {
+        return !zoneDeRecharge;
+    }
+
+    public void ajouterZone() {
+        if (zoneDeRecharge) {
+            System.out.println("Il existe déjà une zone de recharge dans la ville " + nom);
+        } else {
+            zoneDeRecharge = true; // Si aucune zone de recharge n'existe on l'ajoute.
+        }
+    }
+
+    public void retirerZone() {
+        if (!zoneDeRecharge) {
+            System.out.println("Il n'existe pas de zone de recharge dans la ville : " + nom);
+        } else {
+            zoneDeRecharge = false; // Si une zone de recharge existe ont la retire.
+        }
+    }
+}
