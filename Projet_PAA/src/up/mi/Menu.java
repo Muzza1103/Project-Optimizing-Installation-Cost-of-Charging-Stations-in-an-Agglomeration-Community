bb17cb0a40715abcb3b61bb0ed3ec84aea15d0f2
Muzza1103 <145678126+Muzza1103@users.noreply.github.com>
2023-12-22 20:32:31 +0100

    Update Menu.java

--- Projet_PAA/src/up/mi/Menu.java
+++ Projet_PAA/src/up/mi/Menu.java
@@ -2,6 +2,7 @@ package up.mi;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
