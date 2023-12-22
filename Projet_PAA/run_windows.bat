d4e1a472b4fda23f69985f70e495f5acf230af4a
adamowski13 <116688330+adamowski13@users.noreply.github.com>
2023-12-22 22:29:07 +0100

    Update run_windows.bat

--- Projet_PAA/run_windows.bat
+++ Projet_PAA/run_windows.bat
@@ -1,18 +1,13 @@
 @echo off
-set /p JAVAFX_SDK_PATH="Entrez le chemin vers le SDK JavaFX (ex. C:\path\to\javafx-sdk\lib): "
-set /p FILE_PATH="Entrez le chemin du fichier à utiliser (ex. C:\chemin\vers\fichier.txt): "
+SET /P JAVAFX_SDK_PATH="Entrez le chemin vers le SDK JavaFX (par ex. C:\javafx-sdk\lib): "
+SET /P FILE_PATH="Entrez le chemin du fichier à utiliser (par ex. C:\chemin\vers\votre\fichier.txt): "
 
 echo Compilation des fichiers Java...
 javac --module-path "%JAVAFX_SDK_PATH%" --add-modules javafx.controls,javafx.fxml -d bin src\up\mi\*.java
-if NOT %ERRORLEVEL% == 0 goto error
+if %ERRORLEVEL% NEQ 0 (
+    echo Erreur lors de la compilation.
+    exit /b 1
+)
 
 echo Execution de la classe principale...
 java --module-path "%JAVAFX_SDK_PATH%" --add-modules javafx.controls,javafx.fxml -cp bin up.mi.Menu "%FILE_PATH%"
-goto end
-
-:error
-echo Erreur lors de la compilation ou de l'execution.
-
-:end
-pause
-
